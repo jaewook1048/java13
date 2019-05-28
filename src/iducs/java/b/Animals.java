cf3cb29282abc5a3c0d6fa1ed22bdc61e22b879d
jaewook1048 <kosil1048@naver.com>
2019-05-28 17:38:05 +0900

    master revise animal

--- src/iducs/java/b/Animals.java
+++ src/iducs/java/b/Animals.java
@@ -2,7 +2,8 @@ package iducs.java.b;
 
 public interface Animals {
 	String species = "동물"; // public final 
-	
+	String common = "공통점"; // public final 
+
 	void move(); // public abstract
 	void eat();
 	void sound();
